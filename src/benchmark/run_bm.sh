35345b46c5d790bba03f4aa14453e13bea808f94
Aurelian MELINTE <ame01@gmx.net>
2023-10-27 12:27:37 -0400

    Benchmark + perf

--- src/benchmark/run_bm.sh
+++ src/benchmark/run_bm.sh
@@ -11,8 +11,8 @@ googleBenchmark=${HOME}/work/benchmark
 
 target=$(basename -s \.cpp $1)
 
-#TODO: -mavx2 -g
-g++ $1 -std=c++20 -O3 \
+#TODO: -mavx2 
+g++ $1 -std=c++20 -g -O3 \
   -Wall -Wextra -Werror -pedantic -Wno-deperecated-volatile \
   -isystem ${googleBenchmark}/include \
   -L${googleBenchmark}/build/src \
@@ -21,4 +21,8 @@ g++ $1 -std=c++20 -O3 \
   -o ${target} 
 
 ./${target} --benchmark_counters_tabular=true #--benchmark_repetitions=3
+#perf c2c record -g ./${target} --benchmark_counters_tabular=true #--benchmark_repetitions=3
+#perf c2c report 
+#rm perf.data
+
 rm ./${target}
