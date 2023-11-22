b011bf5d73c5f04d9184ff8b7c927c309ab4309b
Aurelian MELINTE <ame01@gmx.net>
2023-11-22 10:08:29 -0500

    llvm MCA stub

---
+++ include/lpt/llvm_mca.hpp
@@ -0,0 +1,38 @@
+/*
+ *  $Id: $
+ *
+ *  Copyright 2023 Aurelian Melinte.
+ *  Released under GPL 3.0 or later.
+ *
+ *  You need clang++.
+ *
+ */
+
+#ifndef INCLUDED_llvm_mca_hpp_7bb266a6_f5ca_4e11_86f6_b5f0e940c5c5
+#define INCLUDED_llvm_mca_hpp_7bb266a6_f5ca_4e11_86f6_b5f0e940c5c5
+
+#pragma once
+
+namespace lpt { namespace llvm {
+
+/*
+ * Machine Code Analyzer wrapper
+ */
+class mca
+{
+public:
+
+    mca()  { LLVM-MCA-BEGIN; }
+    ~mca() { LLVM-MCA-END; }
+
+    mca( const mca& other )            = delete;
+    mca& operator=( const mca& other ) = delete;
+
+    mca( const mca& other )            = delete;
+    mca& operator=( mca&& other )      = delete;
+}; // mca
+
+}} //namespace lpt::llvm
+
+
+#endif //#define INCLUDED_llvm_mca_hpp_7bb266a6_f5ca_4e11_86f6_b5f0e940c5c5
