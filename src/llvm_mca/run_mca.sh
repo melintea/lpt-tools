f1cf15b23331c163cb53f6d839bae14915ab4af2
Aurelian MELINTE <ame01@gmx.net>
2023-12-05 12:11:08 -0500

    LLVM MCA

--- src/llvm_mca/run_mca.sh
+++ src/llvm_mca/run_mca.sh
@@ -17,19 +17,22 @@ lptInc=../../include
 
 target=$(basename -s \.cpp $1)
 
-syntax=unsupported
 processor=`uname -m`
 if [[ ${processor} == *"x86_64"* ]]; then
-  syntax='-mavx2 -mllvm -x86-asm-syntax=intel' 
-fi
-if [[ ${processor} == *"aarch64"* ]]; then
-  syntax= 
+  clang++ $1 -std=c++20 -g -O3 \
+    -Wall -Wextra -Werror -pedantic -Wno-deprecated-volatile \
+    -isystem ${googleBenchmark}/include \
+    -isystem ${lptInc} \
+    -mavx2 -mllvm -x86-asm-syntax=intel \
+    -S -o - | llvm-mca-14 -mcpu=btver2 -timeline
 fi
 
-clang++ $1 -std=c++20 -g -O3 \
-  -Wall -Wextra -Werror -pedantic -Wno-deprecated-volatile \
-  -isystem ${googleBenchmark}/include \
-  -isystem ${lptInc} \
-  ${syntax} \
-  -S -o - | llvm-mca-14 -mcpu=btver2 -timeline
+if [[ ${processor} == *"aarch64"* ]]; then
+  clang++ $1 -std=c++20 -g -O3 \
+    -Wall -Wextra -Werror -pedantic -Wno-deprecated-volatile \
+    -isystem ${googleBenchmark}/include \
+    -isystem ${lptInc} \
+    -mllvm \
+    -S -o - | llvm-mca-14  -timeline
+fi
 
