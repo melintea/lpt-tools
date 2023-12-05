36403c7a5b7738e8272caa0e8730d528dfa3fcb8
Aurelian MELINTE <ame01@gmx.net>
2023-12-05 12:07:16 -0500

    LLVM MCA

--- src/llvm_mca/run_mca.sh
+++ src/llvm_mca/run_mca.sh
@@ -7,16 +7,29 @@ set -x
 # Usage: ./run_mca file.cpp
 # 
 
+if [ $# -ne 1 ]; then
+  echo "Usage: `basename $0` <cpp-file>"
+  exit 1
+fi
+
 googleBenchmark=${HOME}/work/benchmark
 lptInc=../../include
 
 target=$(basename -s \.cpp $1)
 
-#TODO: -mavx2 
+syntax=unsupported
+processor=`uname -m`
+if [[ ${processor} == *"x86_64"* ]]; then
+  syntax='-mavx2 -mllvm -x86-asm-syntax=intel' 
+fi
+if [[ ${processor} == *"aarch64"* ]]; then
+  syntax= 
+fi
+
 clang++ $1 -std=c++20 -g -O3 \
   -Wall -Wextra -Werror -pedantic -Wno-deprecated-volatile \
   -isystem ${googleBenchmark}/include \
   -isystem ${lptInc} \
-  -mavx2 -mllvm -x86-asm-syntax=intel \
+  ${syntax} \
   -S -o - | llvm-mca-14 -mcpu=btver2 -timeline
 
