37f6c14f17ea32b6c028372e4b80a67da5ee364b
Aurelian MELINTE <ame01@gmx.net>
2023-12-06 16:08:59 +0000

    ARM

--- src/llvm_mca/run_mca.sh
+++ src/llvm_mca/run_mca.sh
@@ -24,15 +24,18 @@ if [[ ${processor} == *"x86_64"* ]]; then
     -isystem ${googleBenchmark}/include \
     -isystem ${lptInc} \
     -mavx2 -mllvm -x86-asm-syntax=intel \
-    -S -o - | llvm-mca-14 -mcpu=btver2 -timeline
+    -S -o - \
+  | llvm-mca-14 -mcpu=btver2 -timeline -bottleneck-analysis
 fi
 
+# gcc -E -mcpu=xxx -xc /dev/null
 if [[ ${processor} == *"aarch64"* ]]; then
   clang++ $1 -std=c++20 -g -O3 \
     -Wall -Wextra -Werror -pedantic -Wno-deprecated-volatile \
     -isystem ${googleBenchmark}/include \
     -isystem ${lptInc} \
-    \
-    -S -o - | llvm-mca-14  -timeline
+    -march=armv8.8-a -mcpu=cortex-a76 \
+    -S -o - \
+  | llvm-mca-14  -mcpu=cortex-a76 -mtriple=aarch64 -timeline -bottleneck-analysis
 fi
 
