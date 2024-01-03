82607dcf5b84a57cd382994323128d859a1c366f
Aurelian MELINTE <ame01@gmx.net>
2024-01-03 12:51:22 -0500

    unlikely.cpp

--- src/benchmark/unlikely.cpp
+++ src/benchmark/unlikely.cpp
@@ -1,5 +1,6 @@
 /*
  * un/likely & branchless tests
+ * Test: ./run_bm.sh unlikely.cpp
  */
  
 #include <iostream>
@@ -20,7 +21,7 @@ void BM_branched1(benchmark::State& state) {
     for (size_t i = 0; i < N; ++i) {
         v1[i] = rand();
         v2[i] = rand();
-        c1[i] = rand() & 0x1;
+        c1[i] = rand() % 3; // bools in disguise, 2/3 true
     }
     unsigned long* p1 = v1.data();
     unsigned long* p2 = v2.data();
@@ -50,7 +51,7 @@ void BM_branchless1(benchmark::State& state) {
     for (size_t i = 0; i < N; ++i) {
         v1[i] = rand();
         v2[i] = rand();
-        c1[i] = rand() & 0x1;
+        c1[i] = rand() % 3;  // bools in disguise, 2/3 true
     }
     unsigned long* p1 = v1.data();
     unsigned long* p2 = v2.data();
@@ -79,7 +80,7 @@ void BM_branched2(benchmark::State& state) {
     for (size_t i = 0; i < N; ++i) {
         v1[i] = rand();
         v2[i] = rand();
-        c1[i] = rand() & 0x1;
+        c1[i] = rand() % 3;  // bools in disguise, 2/3 true
     }
     unsigned long* p1 = v1.data();
     unsigned long* p2 = v2.data();
@@ -101,6 +102,36 @@ void BM_branched2(benchmark::State& state) {
     //state.SetBytesProcessed(N*sizeof(unsigned long)*state.iterations());
 }
 
+void BM_branched2_unlikely(benchmark::State& state) {
+    srand(1);
+    const unsigned int N = state.range(0);
+    std::vector<unsigned long> v1(N), v2(N);
+    std::vector<int> c1(N);
+    for (size_t i = 0; i < N; ++i) {
+        v1[i] = rand();
+        v2[i] = rand();
+        c1[i] = rand() % 3;  // bools in disguise, 2/3 true
+    }
+    unsigned long* p1 = v1.data();
+    unsigned long* p2 = v2.data();
+    int* b1 = c1.data();
+    for (auto _ : state) {
+        unsigned long a1 = 0, a2 = 0;
+        for (size_t i = 0; i < N; ++i) {
+            if ( ! b1[i]) [[unlikely]] {
+                a1 += p1[i] - p2[i];
+            } else {
+                a2 += p1[i] * p2[i];
+            }
+        }
+        benchmark::DoNotOptimize(a1);
+        benchmark::DoNotOptimize(a2);
+        benchmark::ClobberMemory();
+    }
+    state.SetItemsProcessed(N*state.iterations());
+    //state.SetBytesProcessed(N*sizeof(unsigned long)*state.iterations());
+}
+
 void BM_branched2_predicted(benchmark::State& state) {
     srand(1);
     const unsigned int N = state.range(0);
@@ -109,7 +140,7 @@ void BM_branched2_predicted(benchmark::State& state) {
     for (size_t i = 0; i < N; ++i) {
         v1[i] = rand();
         v2[i] = rand();
-        c1[i] = rand() > 0;
+        c1[i] = rand() > 0;  // bools in disguise, overhelmlingly true
     }
     unsigned long* p1 = v1.data();
     unsigned long* p2 = v2.data();
@@ -139,7 +170,7 @@ void BM_branchless2(benchmark::State& state) {
     for (size_t i = 0; i < N; ++i) {
         v1[i] = rand();
         v2[i] = rand();
-        c1[i] = rand() & 0x1;
+        c1[i] = rand() % 3;  // bools in disguise, 2/3 true
     }
     unsigned long* p1 = v1.data();
     unsigned long* p2 = v2.data();
@@ -168,6 +199,7 @@ BENCHMARK(BM_branchless1) ARGS;
 BENCHMARK(BM_branched2) ARGS;
 BENCHMARK(BM_branched2_predicted) ARGS;
 BENCHMARK(BM_branchless2) ARGS;
+BENCHMARK(BM_branched2_unlikely) ARGS;
 
 BENCHMARK_MAIN();
 
