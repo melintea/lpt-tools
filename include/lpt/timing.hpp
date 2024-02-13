63ddb259c350b101d58355e75d0b814d01b20179
Aurelian MELINTE <ame01@gmx.net>
2024-02-13 08:10:46 -0500

    deprecate stuff

--- include/lpt/timing.hpp
+++ include/lpt/timing.hpp
@@ -1,14 +1,32 @@
-/* *  Copyright 2012 Aurelian Melinte.  *  Released under GPL 3.0 or later.  * *  You need a C++0x compiler.  *  */#include <chrono>
+/*
+ *  Copyright 2012 Aurelian Melinte. 
+ *  Released under GPL 3.0 or later. 
+ *
+ *  You need a C++0x compiler. 
+ * 
+ */
+
+
+#include <chrono>
 
 #include <lpt/nocopy.hpp>
 
-#pragma once 
+
+#pragma once 
 
 
-namespace lpt { class timing : public nocopy{public:
 
-    typedef std::chrono::milliseconds milliseconds;    timing()
-        : _start(std::chrono::high_resolution_clock::now())    {
+namespace lpt { 
+
+class [[deprecated]] timing : public nocopy
+{
+public:
+
+    typedef std::chrono::milliseconds milliseconds;
+
+    timing()
+        : _start(std::chrono::high_resolution_clock::now())
+    {
     }
 
     template < typename Units >
@@ -16,6 +34,18 @@
     {
         Units us = std::chrono::duration_cast<Units>(std::chrono::high_resolution_clock::now() - _start);
         return us.count();
-    }        ~timing()    {    }private:
+    }
+    
+    ~timing()
+    {
+    }
 
-    std::chrono::high_resolution_clock::time_point _start;};  } //namespace
+
+private:
+
+    std::chrono::high_resolution_clock::time_point _start;
+};
+
+
+  
+} //namespace
