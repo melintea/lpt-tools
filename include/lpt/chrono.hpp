20949632bb6369c19190477441bc5fd76ea2e4a6
Aurelian MELINTE <ame01@gmx.net>
2024-02-14 14:39:00 -0500

    chrono accumulator

--- include/lpt/chrono.hpp
+++ include/lpt/chrono.hpp
@@ -10,6 +10,7 @@
 #pragma once 
 
 #include <chrono>
+#include <functional>
 #include <string>
 
 using namespace std::string_literals;
@@ -25,7 +26,6 @@ public:
     using duration_t  = std::chrono::nanoseconds;
     using percent_t   = double;
 
-    static constexpr const char* unit = "ns";
 
     timepoint() : _point(clock_t::now()) {}
 
@@ -34,6 +34,12 @@ public:
     timepoint& operator=(const timepoint&) = default;
     timepoint(timepoint&&)                 = default;
     timepoint& operator=(timepoint&&)      = default;
+
+    static const std::string& unit()
+    {
+        static const std::string ns{"ns"};
+        return ns;
+    }
 
     auto elapsed() const
     {
@@ -52,7 +58,7 @@ public:
 
     static const std::string& name()
     {
-        static const std::string timeLabel = "ElapsedTime("s + unit + ")"s;
+        static const std::string timeLabel = "ElapsedTime("s + unit() + ")"s;
         return timeLabel;
     }
 
@@ -62,6 +68,8 @@ protected:
     const timepoint_t       _point;
 };
 
+// Do nothing functor
+static void noop(const std::string& /*tag*/, const timepoint::duration_t& /*dur*/) noexcept {}
 template <typename FUNC>
 class measurement 
 {
@@ -97,6 +105,96 @@ private:
     FUNC                    _func;
 
 }; // measurement
+
+/**
+ \code
+    {
+        using namespace std::chrono_literals;
+        lpt::chrono::accumulator acc("Total time");
+        {
+            lpt::chrono::measurement m1(acc.measurement());
+            std::this_thread::sleep_for(1230ms);
+        }
+        {
+            lpt::chrono::measurement m2(acc.measurement());
+            std::this_thread::sleep_for(3210ms);
+        }
+        std::cout << acc << std::endl; // 4440'000'000 ?
+    }
+ \endcode
+*/
+class accumulator 
+{
+public:
+
+    accumulator(std::string tag)
+        : _tag(std::move(tag))
+    { }
+
+    ~accumulator()                             = default;
+
+    accumulator(const accumulator&)            = default;
+    accumulator& operator=(const accumulator&) = default;
+    accumulator(accumulator&&)                 = default;
+    accumulator& operator=(accumulator&&)      = default;
+
+    timepoint::duration_t elapsed() const
+    {
+        return _elapsed;
+    }
+
+    const std::string& tag() const
+    {
+        return _tag;
+    }
+
+    auto measurement() 
+    {
+        return lpt::chrono::measurement(_tag,
+                                        [& acc=*this](auto&& /*tag*/, auto&& dur) -> decltype(auto) {
+                                            acc += dur;
+                                        });
+    }
+
+    accumulator& operator+=(const timepoint::duration_t& dur)
+    {
+        _elapsed += dur;
+        return *this;
+    }
+
+    accumulator& operator+=(const timepoint& tp)
+    {
+        _elapsed += tp.elapsed();
+        return *this;
+    }
+
+    template <typename FUNC>
+    accumulator& operator+=(const class measurement<FUNC>& m)
+    {
+        _elapsed += m.elapsed();
+        return *this;
+    }
+
+    accumulator& operator+=(const accumulator& rhs)
+    {
+        _elapsed += rhs.elapsed();
+        return *this;
+    }
+
+    friend std::ostream& operator<<(std::ostream&      os,
+                                    const accumulator& acc)
+    {
+        os << acc._tag << ": "s 
+           << std::to_string(acc._elapsed.count()) << " "s << timepoint::unit();
+        return os;
+    }
+
+private:
+
+    const std::string       _tag;
+    timepoint::duration_t   _elapsed;
+
+}; // accumulator
 
 }} //namespace lpt::chrono
 
