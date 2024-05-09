3d281c2d7ab9fb183f4942961630699043f7c2a5
Aurelian MELINTE <ame01@gmx.net>
2024-05-09 14:06:07 -0400

    LRU stub

--- src/varia/examples/lru_array.cpp
+++ src/varia/examples/lru_array.cpp
@@ -35,27 +35,32 @@ int main()
     assert(lru.size() == 1);
     std::this_thread::sleep_for(10ms);
 
-    lru.push({"B", 0});
+    lru.push({"B", 1});
     static_assert(lru.max_size() == 5);
     assert(lru.size() == 2);
     std::this_thread::sleep_for(10ms);
 
-    lru.push({"C", 0});
+    lru.push({"C", 2});
     static_assert(lru.max_size() == 5);
     assert(lru.size() == 3);
     std::this_thread::sleep_for(10ms);
 
-    lru.push({"D", 0});
+    lru.push({"D", 3});
     static_assert(lru.max_size() == 5);
     assert(lru.size() == 4);
     std::this_thread::sleep_for(10ms);
 
-    lru.push({"E", 0});
+    lru.push({"E", 4});
     static_assert(lru.max_size() == 5);
     assert(lru.size() == 5);
     std::this_thread::sleep_for(10ms);
 
-    lru.push({"F", 0});
+    lru.push({"A", 5});
+    static_assert(lru.max_size() == 5);
+    assert(lru.size() == 5);
+    std::this_thread::sleep_for(10ms);
+
+    lru.push({"F", 6});  // replace B: A(5)F(6)C(2)D(3)E(4)
     static_assert(lru.max_size() == 5);
     assert(lru.size() == 5);
     std::this_thread::sleep_for(10ms);
