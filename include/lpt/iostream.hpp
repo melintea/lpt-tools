7be5e90deb4c64accca9671f2fe821ffdc39a84b
Aurelian MELINTE <ame01@gmx.net>
2024-08-07 07:43:54 -0400

    iostream.hpp

---
+++ include/lpt/iostream.hpp
@@ -0,0 +1,63 @@
+/*
+ *  $Id: $
+ *
+ *  Copyright 2024 Aurelian Melinte.
+ *  Released under GPL 3.0 or later.
+ *
+ *  You need a C++0x compiler.
+ *
+ */
+
+#ifndef INCLUDED_iostream_hpp_16afdfdb_5c40_473f_9d2c_5fd3cf8fb6f6
+#define INCLUDED_iostream_hpp_16afdfdb_5c40_473f_9d2c_5fd3cf8fb6f6
+
+#pragma once
+
+
+#include <iostream>
+
+
+/**
+ * iostream tools
+ */
+
+namespace lpt {
+
+/// auto-indent ostream
+/*
+
+struct autoindent : private std::streambuf
+                  , public std::ostream
+{
+    autoindent() : std::ostream(this) {}
+
+private:
+
+    int overflow(int c) override
+    {
+        foo(c);
+        return 0;
+    }
+
+
+    void foo(char c)
+    {
+        std::cout.put(c);
+
+    }
+};
+
+int main()
+{
+    autoindent b;
+    b<<"Look a number: "<<std::hex<<29<<std::endl;
+
+    return 0;
+}
+
+*/
+
+} //namespace lpt
+
+
+#endif //#define INCLUDED_iostream_hpp_16afdfdb_5c40_473f_9d2c_5fd3cf8fb6f6
