0738dc3080b8b3cf2f52e7036f3c27590414f5ac
Aurelian MELINTE <ame01@gmx.net>
2025-10-21 08:50:26 -0400

    access.hpp

---
+++ include/lpt/access.hpp
@@ -0,0 +1,24 @@
+/*
+ *  $Id: $
+ *
+ *  Copyright 2025 Aurelian Melinte.
+ *  Released under GPL 3.0 or later.
+ *
+ */
+
+#ifndef INCLUDED_access_hpp_0a96beab_1f4e_4307_b634_ff9181149b2e
+#define INCLUDED_access_hpp_0a96beab_1f4e_4307_b634_ff9181149b2e
+
+#pragma once
+
+namespace lpt {
+
+/*
+ * Access to private members
+ * @see https://github.com/dfrib/accessprivate/tree/master
+ */
+
+} //namespace lpt
+
+
+#endif //#define INCLUDED_access_hpp_0a96beab_1f4e_4307_b634_ff9181149b2e
